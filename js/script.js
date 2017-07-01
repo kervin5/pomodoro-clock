f019a2559c33dfdad46625464d3b619460ae08ab
kervin5 <kervinlvh@gmail.com>
2017-06-30 22:16:40 -0700

    Fix time interval issue

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 var timerOn = false;
 var currentTimer = "session";
 var isPaused = false;
+var initialValue = 60;
+var currentTime = 0;
+var interval;
 
 function startTimer(duration, displayMinute, displaySecond, message) {
     var bgPosition = duration;
@@ -11,42 +14,40 @@ function startTimer(duration, displayMinute, displaySecond, message) {
 
     $('.info').text(message);
     var timeIntervalID = setInterval(function () {
+        interval = timeIntervalID;
+            if(!isPaused) {
+                currentTime = timer;
+                console.log(currentTime);
+                bgPosition = 110 - ((timer / initialValue) + 0.1) * 100;
+                $(".bg-group").css("top", bgPosition + "%");
+                minutes = parseInt(timer / 60, 10);
 
-        if(!isPaused){
-            console.log(timer)
-            bgPosition = 110 - ((timer / duration) + 0.1)*100;
-            console.log(timer);
-            $(".bg-group").css("top",bgPosition+"%");
-            minutes = parseInt(timer / 60, 10);
+                seconds = parseInt(timer % 60, 10);
 
-            seconds = parseInt(timer % 60, 10);
+                minutes = minutes < 10 ? "0" + minutes : minutes;
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+                displayMinute.textContent = minutes;
 
-            displayMinute.textContent = minutes;
+                displaySecond.textContent = seconds;
 
-            displaySecond.textContent = seconds;
 
-            if (--timer < 0) {
+                if (--timer < 0) {
 
-                timer = 0;
+                    timer = 0;
 
-                if (timer === 0 && currentTimer !== "break") {
+                    if (timer === 0 && currentTimer !== "break") {
+                        initialValue = 120;
+                        clearInterval(timeIntervalID);
+                        startTimer(120, displayMinute, displaySecond, "Break started");
+                        currentTimer = "break";
+                    } else {
+                        $('.info').text("Break is over");
+                    }
 
-                    clearInterval(timeIntervalID);
-                    startTimer(120,displayMinute,displaySecond,"Break started");
-                    currentTimer = "break";
-                } else {
-                    $('.info').text("Break is over");
                 }
-
             }
-        }
-
-
-
     }, 1000);
 
 }
@@ -101,14 +102,18 @@ $(document).ready(function () {
     $(".play").click(function () {
         $(".play").fadeOut();
         $(".pause").delay(400).fadeIn();
-        if(!timerOn){
+        if(!timerOn && !isPaused){
             startTimer(fragmentTime, displayMinute, displaySecond,"Session started");
+            timerOn = true;
+        } else {
+            startTimer(currentTime,displayMinute,displaySecond,"Session started")
         }
         isPaused = false;
     });
     $(".pause").click(function () {
         $(".pause").fadeOut();
         $(".play").delay(400).fadeIn();
+        clearInterval(interval);
         isPaused = true;
     });
 });
